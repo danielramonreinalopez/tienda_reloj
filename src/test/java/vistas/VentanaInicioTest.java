e9bcebb341fdddf01405c9e55c989a9cbf89f063
Jorge Ruiz <ruiz.jorge.9825@eam.edu.co>
2024-10-09 20:35:50 -0500

    se agrego dependencias de junit

---
+++ src/test/java/vistas/VentanaInicioTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package vistas;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorge
+ */
+public class VentanaInicioTest {
+    
+    public VentanaInicioTest() {
+    }
+
+    @org.junit.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        VentanaInicio.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
